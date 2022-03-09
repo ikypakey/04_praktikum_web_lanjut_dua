fa9429444d7107598177d8d61160e4cb46293ddb
masbarok193@gmail.com <masbarok193@gmail.com>
2022-03-09 22:42:15 +0700

    Modify on post

--- database/migrations/2022_03_09_152053_create_posts_table.php
+++ database/migrations/2022_03_09_152053_create_posts_table.php
@@ -13,7 +13,7 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+       Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->String('gambar');
             $table->String('nama');
