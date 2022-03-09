88c169ff3cb9d1b31366fbf87e2f168f726f69fc
masbarok193@gmail.com <masbarok193@gmail.com>
2022-03-09 22:35:56 +0700

    Insert data on blog

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BlogSeeder extends Seeder
 {
@@ -13,6 +14,31 @@ class BlogSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('blogs')->insert([
+            [
+                'gambar'=>'gambar1.jpg',
+                'nama'=>'Gunung bromo',
+                'tanggal'=>'May 30, 2022',
+                'keterangan'=>'Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.'
+            ],
+            [
+                'gambar'=>'gambar2.jpg',
+                'nama'=>'Danau Ranu',
+                'tanggal'=>'Feb 25, 2022',
+                'keterangan'=>'Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.'
+            ],
+            [
+                'gambar'=>'gambar3.jpeg',
+                'nama'=>'Gunung arjuno',
+                'tanggal'=>'Jan 15, 2022',
+                'keterangan'=>'Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.'
+            ],
+            [
+                'gambar'=>'gambar4.jpg',
+                'nama'=>'Candi borobudur',
+                'tanggal'=>'april 27, 2022',
+                'keterangan'=>'Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.'
+            ]
+            ]);
     }
 }
