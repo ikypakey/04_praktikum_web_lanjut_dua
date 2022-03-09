3aa30e7a5391bb4e47d67e129d5b69ee2e4e09cb
masbarok193@gmail.com <masbarok193@gmail.com>
2022-03-09 23:00:40 +0700

    Calling Model in Controller Post Details

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            BlogSeeder::class
+            BlogSeeder::class,
+            PostSeeder::class
         ]);
     }
 }
