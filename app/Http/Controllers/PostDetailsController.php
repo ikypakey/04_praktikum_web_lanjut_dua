71d8fd1f9dad4781f52ccd93f89a2197efce8ef6
masbarok193@gmail.com <masbarok193@gmail.com>
2022-03-09 22:55:07 +0700

    Calling model on controller Post Details

--- app/Http/Controllers/PostDetailsController.php
+++ app/Http/Controllers/PostDetailsController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostDetailsController extends Controller
 {
     public function postDetail(){
         return view('post-details',[
-            'tittle' => 'Post Details Page'
+            'title' => 'Post Detail',
+            'posts' => Post::all()
         ]);
     }
 }
